8dc9da08229dab00a40ca164b2eb553006087e8b
Ivan <2087678V@student.gla.ac.uk>
2015-11-23 23:32:49 +0000

    get a list of restaurants based on location and cuisine

--- main.js
+++ main.js
@@ -11,19 +11,40 @@ $(document).ready(function(){
             }
     });
 
-
-	$( "#button" ).click(getRestaurants("g20"));
+	$( "#button" ).click(getRestaurants("G41 1BZ","Desserts"));
 });
+    
 
-    getRestaurants = function (postcode) {
+    getRestaurants = function (postcode, cuisine) {
         console.log('getting restaurants for postcode: ' + postcode);
-
+        
         $.getJSON(baseURI + 'restaurants', {
                 q: postcode.replace(' ', ''),
-                c: "Chinese"
+                c: cuisine,               
         },
             function (results) {
                 console.log(results);
+                
+                //get array of resturant based on specific cuisine
+                var cuisineRestaurants=jQuery.grep(results.Restaurants, function (a){
+                    for(i = 0; i<a.CuisineTypes.length; i++){
+                        if (a.CuisineTypes[i].Name==cuisine) return true;
+                    }
+                    return false;
+                });
+                console.log(cuisineRestaurants);
+                
+                //sort rests in descending order based on rating
+                var sortedRestaurants = cuisineRestaurants.sort(function(a, b){
+                    var a1= a.RatingStars, b1= b.RatingStars;
+                    if(a1== b1) return 0;
+                    return a1> b1? -1: 1;
+                });
+                
+                //select rests based on postcode
+                console.log(jQuery.grep(sortedRestaurants, function (a){
+                    return a.Postcode.indexOf(postcode.toUpperCase())> -1;
+                }));
             }
         );
     };
