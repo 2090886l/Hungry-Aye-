a85ee0236b81948c97d3728de4653ad08ed974b8
Ivaylo Lafchiev <ivailolafchiev@yahoo.com>
2015-11-23 21:12:21 +0000

    wokring jquery for postcode justeat

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 console.log("asda");
-console.log($("#test"))
-$('#test').empty();
-console.log("asdaasdasd");
+console.log($("#test"));
+var baseURI = 'https://public.je-apis.com/';
+
+$(document).ready(function(){
+
+	$.ajaxSetup({
+            headers: {
+                'Accept-Tenant': 'uk',
+                'Authorization': 'Basic VGVjaFRlc3RBUEk6dXNlcjI=',
+            }
+    });
+
+
+	$( "#button" ).click(getRestaurants("g20"));
+});
+
+    getRestaurants = function (postcode) {
+        console.log('getting restaurants for postcode: ' + postcode);
+
+        $.getJSON(baseURI + 'restaurants', {
+                q: postcode.replace(' ', ''),
+                c: "Chinese"
+        },
+            function (results) {
+                console.log(results);
+            }
+        );
+    };
