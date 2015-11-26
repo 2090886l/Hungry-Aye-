df3d6ca62ecdb0cd721768755b01100ad3fc446f
Elldorin <anton.mtodorov@gmail.com>
2015-11-26 22:26:49 +0000

    Mega mn promeni

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ $(document).ready(function(){
         var restaurant = $("#restaurant").is(':checked');
         var takeAway = $("#takeAway").is(':checked');
         getRestaurants(postCode, cuisine);
+
+
         $('#query-results').fadeIn(800);
         $("h1").animate({"margin-top":"2%"}, 800);
         $("#main-panel").animate({"margin-top":"2%"}, 800);
@@ -32,7 +34,6 @@ $(document).ready(function(){
                 c: cuisine,               
         },
             function (results) {
-                console.log(results);
                 
                 //get array of resturant based on specific cuisine
                 var cuisineRestaurants=jQuery.grep(results.Restaurants, function (a){
@@ -41,7 +42,6 @@ $(document).ready(function(){
                     }
                     return false;
                 });
-                console.log(cuisineRestaurants);
                 
                 //sort rests in descending order based on rating
                 var sortedRestaurants = cuisineRestaurants.sort(function(a, b){
@@ -51,9 +51,52 @@ $(document).ready(function(){
                 });
                 
                 //select rests based on postcode
-                console.log(jQuery.grep(sortedRestaurants, function (a){
+                var resultsByCode = jQuery.grep(sortedRestaurants, function (a){
                     return a.Postcode.indexOf(postcode.toUpperCase())> -1;
-                }));
+                });
+                console.log(resultsByCode[1]);
+                $('#query-results').empty();
+                for (i = 0; i<resultsByCode.length; i++) {
+                    if (resultsByCode[i].IsOpenNow) {
+                        var open = "open.jpg";
+                    } else {
+                        var open = "closed.jpg";
+                    }
+                    if (resultsByCode[i].IsOpenNowForDelivery) {
+                        var delivery = "open.jpg";
+                    } else {
+                        var delivery = "closed.jpg";
+                    }
+                    if (resultsByCode[i].IsHalal) {
+                        var halal = "open.jpg";
+                    } else {
+                        var halal = "closed.jpg";
+                    }
+
+                    $('#query-results').append('<div class="query-result">' +
+                        '<img src="' +
+                        resultsByCode[i].Logo[0].StandardResolutionURL +
+                        '"class="img-circle" width="20%" height="20%"></img>' +
+                        '<ul class="list-group info">' +
+                        '<li class="list-group-item">Name: ' +
+                        resultsByCode[i].Name +
+                        '<li class="list-group-item">Address: ' +
+                        resultsByCode[i].Address +
+                        '<li class="list-group-item">Open: ' +
+                        '<img class="available-image " src="' +
+                        open +
+                        '"</img>' +
+                        '<li class="list-group-item">Delivery now: ' +
+                        '<img class="available-image " src="' +
+                        delivery +
+                        '"</img>' +
+                        '<li class="list-group-item">Halal: ' +
+                        '<img class="available-image " src="' +
+                        halal +
+                        '"</img>' +
+                        '</ul>' +
+                        '</div>');
+                }
             }
         );
     };
